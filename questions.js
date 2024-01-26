0bcb223cec1f06e6ac71e8eac2bfe5512bf9f894
Aquita Bryant <33906068+She-Who-Remains@users.noreply.github.com>
2024-01-26 10:48:39 -0600

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,63 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Stich's Experiment Number",
+        answer: "C. 626",
+        options:[
+            "A. R2D2",
+            "B. 21",
+            "C. 626",
+            "D. 111"
+        ],
+        img: "./images/lil01.png"
+    },
+    {
+        numb: 2,
+        question: "'I'd surf til the sun sest beyond the horizon' - are lyrics sung in this song",
+        answer: "A. Hawaiian Roller Coaster Ride",
+        options:[
+            "A. Hawaiian Roller Coaster Ride",
+            "B. Ohana",
+            "C. Rock the Boat",
+            "D. Hawaiian High"
+        ],
+        img: "./images/lilo2.png"
+    },
+    {
+        numb: 3,
+        question: "This is how much 'Lilo' paid for Stich when adopting him",
+        answer: "C. $2",
+        options:[
+            "A. $5",
+            "B. $6.26",
+            "C. $2",
+            "D. He was free"
+        ],
+        img: "./images/lilo3.jpg"
+    },
+    {
+        numb: 4,
+        question: "After Lilo tells Stitch to make something for a change, he build a model version of this city before destroying it",
+        answer: "D. San Francisco",
+        options:[
+            "A. New York",
+            "B. Los Angeles",
+            "C. Seattle",
+            "D. San Francisco"
+        ],
+        img: "./images/lilo4.png"
+    },
+    {
+        numb: 5,
+        question: "After Stich steal's Mertiles's tri-cycle, Jumba says Stitch's destructive programming is taking effect. Meaning he'll soon be drawn to large cities. Backing up sewers, reversing street signs, and doing this to everyone",
+        answer: "D. Stealing Their Left Shoe",
+        options:[
+            "A. Stealing Their Red Clothes out of the Dryer",
+            "B. Stealing Their Rubberbands",
+            "C. Stealing Their Mail",
+            "D. Stealing Their Left Shoe"
+        ],
+        img: "./images/lilo5.jpg"
+    }
+    
+];
